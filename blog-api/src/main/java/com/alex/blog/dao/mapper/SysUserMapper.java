09f3ce959d8beb15f7849cb5f1cf0607575e0407
809568883@qq.com <qwe123>
2022-03-13 15:58:10 +0800

    2st

--- blog-api/src/main/java/com/alex/blog/dao/mapper/SysUserMapper.java
+++ blog-api/src/main/java/com/alex/blog/dao/mapper/SysUserMapper.java
@@ -2,10 +2,12 @@ package com.alex.blog.dao.mapper;
 
 import com.alex.blog.dao.pojo.SysUser;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
  * @author Alexandermucc
  * @date 2022/3/12 - 17:49 - 周六
  **/
+
 public interface SysUserMapper extends BaseMapper<SysUser> {
 }
