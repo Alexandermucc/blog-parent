cb0599d6713eb46783be56310b0ab86dadcd77d8
alexandermucc <809568883@qq.com>
2022-03-14 20:59:01 +0800

    p19

---
+++ blog-api/src/main/java/com/alex/blog/dao/mapper/ArticleBodyMapper.java
@@ -0,0 +1,8 @@
+package com.alex.blog.dao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.alex.blog.dao.pojo.ArticleBody;
+
+public interface ArticleBodyMapper extends BaseMapper<ArticleBody> {
+}
+
